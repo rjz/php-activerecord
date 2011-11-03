d7e5eacae9b9d26d60419fb3dd031a2d93ddb38a
RJ Zaworski <rj@rjzaworski.com>
2011-11-03 10:02:13 -0700

    fixed quotes in REGEXP_REPLACE

--- lib/adapters/PgsqlAdapter.php
+++ lib/adapters/PgsqlAdapter.php
@@ -41,7 +41,7 @@ SELECT a.attname AS field, a.attlen,
 REPLACE(pg_catalog.format_type(a.atttypid, a.atttypmod),'character varying','varchar') AS type,
 a.attnotnull AS not_nullable, 
 i.indisprimary as pk,
-REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(s.column_default,'::[a-z_ ]+',''),'\'$',''),'^\'','') AS default
+REGEXP_REPLACE(REGEXP_REPLACE(REGEXP_REPLACE(s.column_default,'::[a-z_ ]+',''),'''$',''),'^''','') AS default
 FROM pg_catalog.pg_attribute a
 LEFT JOIN pg_catalog.pg_class c ON(a.attrelid=c.oid)
 LEFT JOIN pg_catalog.pg_index i ON(c.oid=i.indrelid AND a.attnum=any(i.indkey))
@@ -51,6 +51,8 @@ AND a.attnum > 0
 AND NOT a.attisdropped
 ORDER BY a.attnum
 SQL;
+
+
 		$values = array($table,$table);
 		return $this->query($sql,$values);
 	}
